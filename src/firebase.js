8479c348bec9cda14d70d62e22baee10dccf01da
ayushshah31 <shahayush934@gmail.com>
2022-09-08 00:47:39 +0530

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyB71-xdTSPeG4rDgjCUax_SSkBn9rGUPFU",
+  authDomain: "portfolio-49294.firebaseapp.com",
+  projectId: "portfolio-49294",
+  storageBucket: "portfolio-49294.appspot.com",
+  messagingSenderId: "728865260315",
+  appId: "1:728865260315:web:ef7cd7012fa542360009be"
 };
 
 const app = initializeApp(firebaseConfig);
